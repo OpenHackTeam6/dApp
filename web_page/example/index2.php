19e4a7d70342633a614ba1bdcd671273a4cf3361
HankFrancis <tqkrghkco@gmail.com>
2018-06-29 15:22:53 +0900

    web page

---
+++ web_page/example/index2.php
@@ -0,0 +1,30 @@
+
+
+
+<?php
+
+
+
+		$data = $_POST[test2];
+        $_SESSION[org] = $_POST[test2];
+
+
+
+foreach ($_POST[test2] as $selectedOption)
+    echo $selectedOption."\n";
+
+
+?>
+
+
+<html>
+
+
+sdfs
+
+<?=$data?>
+<?=$_SESSION[org]?>
+
+
+</html>
+
