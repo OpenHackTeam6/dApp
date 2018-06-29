32ac261ed95b817f795b55c906ce89664e7526c2
HankFrancis <tqkrghkco@gmail.com>
2018-06-29 17:13:18 +0900

    table loading page added

--- web_page/example/loading.php
+++ web_page/example/loading.php
@@ -1,21 +1,57 @@
 <!doctype html>
 <html>
+
+
+
+
 <head>
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>jQuery scroll-navigation Plugin Demo</title>
         <link rel="stylesheet" href="style.css" />
-        
-
         <style>
         #container { max-width:640px; margin:70px auto;  color:black;}
         </style>
 
+        
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/css/select2.min.css" rel="stylesheet" />
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/js/select2.min.js"></script>
+        <script>
+          $(document).ready(function() {
+    $('.js-example-basic-multiple').select2();
+});</script>
+<style>
+.loader {
+  border: 16px solid #f3f3f3;
+  border-radius: 50%;
+  border-top: 16px solid #3498db;
+  width: 120px;
+  height: 120px;
+  -webkit-animation: spin 2s linear infinite; /* Safari */
+  animation: spin 2s linear infinite;
+}
 
+/* Safari */
+@-webkit-keyframes spin {
+  0% { -webkit-transform: rotate(0deg); }
+  100% { -webkit-transform: rotate(360deg); }
+}
 
+@keyframes spin {
+  0% { transform: rotate(0deg); }
+  100% { transform: rotate(360deg); }
+}
+</style>
     </head>
     <body>
+
+
+   
+
+
+
         <div id="header">
             <div id="logo">문서 발급</div>
             <ul id="tabs" class='nav'>
@@ -27,10 +63,13 @@
             </ul>
           </div>
 
-          <div id="container" class="scrollNavData">
+          <div id="container" class="scrollNavData">  
             <div id="i1" class="scrollNav-content">
               <div id="d1">
-                <h2>주민등록표등본 전송</h2>
+
+이영진님의
+
+
 <?php
 
 
@@ -46,37 +85,15 @@ $_SESSION[docu] = $_POST['documents'];
 if(isset($_SESSION[docu])){ // select_name will be replaced with your input filed name
   $getInput = $_SESSION[docu]; // select_name will be replaced with your input filed name
   $selectedOption = "";
-  ?>
 
-
-
-<div>
-<table cellspacing="1" style="width:1000px;height:50px;border:0px;background-color:#999999;">
-<tr>
-        <td align="center" valign="middle" width="30%" style="height:30px;background-color:#CCCCCC;">전송된 문서</td>
-        <td align="center" valign="middle" width="55%" style="height:30px;background-color:#CCCCCC;">전송 기관</td>
-        <td align="center" valign="middle" width="15%" style="height:30px;background-color:#CCCCCC;">접수 여부</td>
-        
-    </tr>
-<?php
   foreach ($getInput as $option => $value) {
-
-    echo $value;
-    echo '<>';
 
     $selectedOption .= $value.','; // I am separating Values with a comma (,) so that I can extract data using explode()
   
 
   ?>
 
-   <tr>
-    
-        <td align="center" valign="middle" style="height:30px;background-color:#FFFFFF;"><?=$value?></td>
-        <td align="center" valign="middle" style="height:30px;background-color:#FFFFFF;"><?=$data?></td>
-        <td align="center" valign="middle" style="height:30px;background-color:#FFFFFF;">접수중</td>
-       
-    </tr>
-
+        [ <?=$value?> ]
 
 <?php
 
@@ -84,16 +101,26 @@ if(isset($_SESSION[docu])){ // select_name will be replaced with your input file
   
 }
 
-
 ?>
 
-</table>
+
+
+              
+             를<?=$_SESSION[org]?>으로 아주 안전하게 보내는 중이에요.
 
 
 
 
 
-    
+
+
+
+              <h2>How To Create A Loader</h2>
+
+              <div class="loader" align="center"></div>
+                
+
+      
     </body>
 </html>
 
