8ecf25d41c133cc30433f14de3aa2f9e143e8a20
eric4266 <eric4266@users.noreply.github.com>
2018-06-29 14:33:51 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
-var web3 = new Web3();
-var provider = new web3.providers.HttpProvider("http://localhost:8545");
+var web3 = require('web3');
+var request = require('request');
+var express = require('express');
+var bodyParser = require('body-parser');
+var fs = require('fs');
+var Tx = require('ethereumjs-tx');
 
-app.get("/submit", function(req, res) {
+var app = express();
+var hostname = "http://127.0.0.1";
+
+var web3 = new web3(new web3.providers.HttpProvider('http://localhost:8545'));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/',function(req,res){
+    fs.readFile('index.html',function(error,data){
+        res.writeHead(200,{'Content-Type':'text/html'});
+        res.end(data);
+    });
+});
+
+app.post("/action", function(req, res) {
 
 });
